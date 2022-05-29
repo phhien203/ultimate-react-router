cb3bc3b1c076a8923f69721de1d7d32e9c9c4f82
Pham Huu Hien <phhien203@gmail.com>
2022-05-29 21:25:49 +0700

    Fetch data for ProductsIndex

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { listProducts } from "./ProductsService";
 
 const ProductsIndex = () => {
-  return <div>List of products...</div>;
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await listProducts();
+      setProducts(data);
+    })();
+  }, []);
+
+  if (products === null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductsIndex;
