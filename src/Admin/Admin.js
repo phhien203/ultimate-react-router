3df4d45d3475b5f9db4bebccf5d6ab205f2fbaf0
Pham Huu Hien <phhien203@gmail.com>
2022-05-31 19:03:36 +0700

    Add Admin child route

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-const Admin = () => <div>Admin</div>;
+import ProductsIndex from "../Products/ProductsIndex";
+import ProductDetails from "../Products/ProductDetails";
+import ProductEdit from "../Products/ProductEdit";
+import { css } from "@emotion/css";
+
+const AdminStyles = css`
+  .Admin {
+    &-Header {
+      display: flex;
+      align-items: center;
+    }
+    &-New {
+      text-decoration: none;
+      border: 2px solid #fff;
+      color: #fff;
+      padding: 4px 10px;
+      border-radius: 6px;
+      font-weight: 600;
+      text-transform: uppercase;
+      margin-left: auto;
+    }
+  }
+`;
+
+const Admin = () => (
+  <div className={AdminStyles}>
+    <div className="Admin-Header">
+      <h1>Admin</h1>
+      <Link to="new" className="Admin-New">New</Link>
+    </div>
+    <Routes>
+      <Route path="/" element={<ProductsIndex />} />
+      <Route path="new" element={<ProductEdit />} />
+      <Route path=":id" element={<ProductDetails />} />
+    </Routes>
+  </div>
+);
 
 export default Admin;
